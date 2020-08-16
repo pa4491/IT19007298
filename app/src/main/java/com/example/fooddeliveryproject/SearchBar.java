85fc6706553abf4a8e40b74d3cb84de20122153a
Akashkavinda <akashpalandagama@gmail.com>
2020-08-17 01:36:12 +0530

    Content Management of Food Delivery App

---
+++ app/src/main/java/com/example/fooddeliveryproject/SearchBar.java
@@ -0,0 +1,14 @@
+package com.example.fooddeliveryproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_bar);
+    }
+}
